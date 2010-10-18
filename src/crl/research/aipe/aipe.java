56fd6d445bd60c6f170b704a18806c490523f8ae
Dr. Miguel Alonso Jr. <Dr. Miguel Alonso Jr.@localhost>
2010-10-18 10:00:06 -0400

    Added buttons and logo

--- src/crl/research/aipe/aipe.java
+++ src/crl/research/aipe/aipe.java
@@ -1,7 +1,12 @@
 package crl.research.aipe;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class aipe extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,31 @@ public class aipe extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        TextView txt = (TextView) findViewById(R.id.TextView01);  
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/DroidLogo.ttf");  
+        txt.setTypeface(font);
+        
+        ((Button) findViewById(R.id.Button01)).setOnClickListener(mSelectListener);
+        ((Button) findViewById(R.id.Button02)).setOnClickListener(mProcessListener);
+        ((Button) findViewById(R.id.Button03)).setOnClickListener(mResetListener);
+        
+     }
+    
+    OnClickListener mSelectListener = new OnClickListener() {
+        public void onClick(View v) {
+            finish();
+        }
+    };
+    
+    OnClickListener mProcessListener = new OnClickListener(){
+    	public void onClick(View v){
+    		finish();
+    	}
+    };
+    
+    OnClickListener mResetListener = new OnClickListener(){
+    	public void onClick(View v){
+    		finish();
+    	}
+    };
 }
