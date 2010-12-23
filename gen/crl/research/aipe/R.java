9bb0bdfbc49b5affd4ac0fa693b89f414362d08e
Dr. Miguel Alonso Jr. <Dr. Miguel Alonso Jr.@localhost>
2010-12-23 14:51:01 -0500

    Removed tracking of binaries

--- gen/crl/research/aipe/R.java
+++ gen/crl/research/aipe/R.java
@@ -16,21 +16,20 @@ public final class R {
     }
     public static final class id {
         public static final int Button01=0x7f050002;
-        public static final int Button02=0x7f050004;
-        public static final int Button03=0x7f050003;
+        public static final int Button02=0x7f050005;
         public static final int RelativeLayout01=0x7f050000;
-        public static final int TextView01=0x7f050005;
+        public static final int Spinner01=0x7f050003;
+        public static final int TextView01=0x7f050004;
         public static final int mainImageView=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int aipe=0x7f040005;
+        public static final int aipe=0x7f040004;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
-        public static final int process=0x7f040003;
-        public static final int reset=0x7f040004;
-        public static final int select=0x7f040002;
+        public static final int load=0x7f040002;
+        public static final int reset=0x7f040003;
     }
 }
